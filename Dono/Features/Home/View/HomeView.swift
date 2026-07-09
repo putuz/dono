53cdee563937cfd6e7f2dbc8227d58a615b2cc80
Bahtiar <pututyb@gmail.com>
2026-07-09 11:33:10 +0700

    Create HomeView Screen

---
+++ Dono/Features/Home/View/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  Dono
+//
+//  Created by Bahtiar on 09/07/26.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeView()
+}
