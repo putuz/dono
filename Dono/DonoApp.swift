d64ae3a121c9737b5d073fbca188717eb806de45
Bahtiar <pututyb@gmail.com>
2026-07-09 10:37:12 +0700

    Initial Commit

---
+++ Dono/DonoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DonoApp.swift
+//  Dono
+//
+//  Created by Bahtiar on 09/07/26.
+//
+
+import SwiftUI
+
+@main
+struct DonoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
